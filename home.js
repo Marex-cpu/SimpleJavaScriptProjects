e9fb2f8fb945da43b6e8955d943c9eb3ae06eb75
marko-petrovic <markoo3871@gmail.com>
2022-05-11 11:41:53 +0200

    add: Menu App

---
+++ home.js
@@ -0,0 +1,64 @@
+//sections
+const section = [
+  {
+    number: 1,
+    nameProject: "Color Flipper",
+    link: "/ColorFlipper/main.html",
+    img: "/images/colorflipper.png",
+  },
+  {
+    number: 2,
+    nameProject: "Counter",
+    link: "/Counter/app.html",
+    img: "/images/counter.png",
+  },
+  {
+    number: 3,
+    nameProject: "Modal",
+    link: "/Modal/app.html",
+    img: "/images/modal.png",
+  },
+  {
+    number: 4,
+    nameProject: "Reviews",
+    link: "/Reviews/app.html",
+    img: "/images/reviews.png",
+  },
+  {
+    number: 5,
+    nameProject: "Questions",
+    link: "/Questions/app.html",
+    img: "/images/questions.png",
+  },
+  {
+    number: 6,
+    nameProject: "Menu",
+    link: "/Menu/app.html",
+    img: "/images/menu.png",
+  },
+];
+
+const wrapper = document.querySelector(".wrapper");
+
+//load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayAllSections(section);
+});
+
+function displayAllSections(sectionItems) {
+  let displaySection = sectionItems.map(function (item) {
+    return `<section class="container">
+        <div class="line">
+          <p class="number">${item.number}.</p>
+          <p class="name-project">${item.nameProject}</p>
+        </div>
+        <article class="document">
+          <a href=${item.link}>
+            <img class="img" src=${item.img} alt="img-site" />
+          </a>
+        </article>
+      </section>`;
+  });
+  displaySection = displaySection.join("");
+  wrapper.innerHTML = displaySection;
+}

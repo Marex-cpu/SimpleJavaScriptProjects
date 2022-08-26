db23586454b50e91b306e3685d55f2308a28cd11
marko-petrovic <markoo3871@gmail.com>
2022-08-26 12:21:43 +0200

    add: hamburger menu, links-container

--- home.js
+++ home.js
@@ -6,6 +6,39 @@ window.addEventListener("load", function () {
   }, 1000);
 });
 
+//hamburger menu
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const linksContainer = document.querySelector(".links-container");
+
+let menuOpen = false;
+
+hamburgerMenu.addEventListener("click", () => {
+  linksContainer.classList.toggle("show-menu");
+
+  if (!menuOpen) {
+    hamburgerMenu.classList.add("open");
+    menuOpen = true;
+  } else {
+    hamburgerMenu.classList.remove("open");
+    menuOpen = false;
+  }
+});
+
+//links container
+const links = document.querySelectorAll(".link-item");
+links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    if (!menuOpen) {
+      hamburgerMenu.classList.add("open");
+      menuOpen = true;
+    } else {
+      hamburgerMenu.classList.remove("open");
+      menuOpen = false;
+    }
+    linksContainer.classList.remove("show-menu");
+  });
+});
+
 //sections
 const section = [
   {
@@ -13,54 +46,63 @@ const section = [
     nameProject: "Color Flipper",
     link: "/ColorFlipper/main.html",
     img: "/images/colorflipper.png",
+    id: "colorflipper",
   },
   {
     number: 2,
     nameProject: "Counter",
     link: "/Counter/app.html",
     img: "/images/counter.png",
+    id: "counter",
   },
   {
     number: 3,
     nameProject: "Modal",
     link: "/Modal/app.html",
     img: "/images/modal.png",
+    id: "modal",
   },
   {
     number: 4,
     nameProject: "Reviews",
     link: "/Reviews/app.html",
     img: "/images/reviews.png",
+    id: "reviews",
   },
   {
     number: 5,
     nameProject: "Questions",
     link: "/Questions/app.html",
     img: "/images/questions.png",
+    id: "questions",
   },
   {
     number: 6,
     nameProject: "Menu",
     link: "/Menu/app.html",
     img: "/images/menu.png",
+    id: "menu",
   },
   {
     number: 7,
     nameProject: "Nav",
     link: "/Nav/app.html",
     img: "/images/nav.png",
+    id: "nav",
   },
   {
     number: 8,
     nameProject: "Tabs",
     link: "/Tabs/app.html",
     img: "/images/tabs.png",
+    id: "tabs",
   },
   {
     number: 9,
     nameProject: "Countdown",
     link: "/Countdown/app.html",
     img: "/images/countdown.png",
+    id: "countdown",
   },
 ];
 const wrapper = document.querySelector(".wrapper");
@@ -72,7 +114,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
 function displayAllSections(sectionItems) {
   let displaySection = sectionItems.map(function (item) {
-    return `<section class="container">
+    return `<section id='${item.id}' class="container">
         <div class="line">
           <p class="number">${item.number}.</p>
           <p class="name-project">${item.nameProject}</p>
